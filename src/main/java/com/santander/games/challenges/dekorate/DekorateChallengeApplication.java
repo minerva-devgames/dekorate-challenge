7d1eb09c4744c6a5dc64894f998ac5ae03ea97bb
user6 <ignacio.martinmartin@gruposantander.com>
2021-09-30 14:41:00 +0000

    add jib plugin

--- src/main/java/com/santander/games/challenges/dekorate/DekorateChallengeApplication.java
+++ src/main/java/com/santander/games/challenges/dekorate/DekorateChallengeApplication.java
@@ -3,6 +3,8 @@ package com.santander.games.challenges.dekorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.jib.annotation.JibBuild;
+
 @SpringBootApplication
 public class DekorateChallengeApplication {
 
